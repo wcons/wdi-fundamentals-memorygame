cb79f350cb59318fe4b5f1a77c5cd11db8d33f76
William Constantino <wconstantino@Williams-MacBook-Air.local>
2019-04-25 02:50:36 -0400

    created game logic

--- memorygame/js/main.js
+++ memorygame/js/main.js
@@ -1,6 +1,15 @@
-cardOne = "queen"
-cardTwo = "king"
-cardThree = "queen"
-cardFour = "king"
-console.log("User flipped " + cardOne)
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+var cardOne = cards[0];
+var cardTwo = cards[2];
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
+    alert("You found a match!");
+}
+ else {
+        alert("Sorry, try again");
+    }
+
+console.log("User flipped " + cardOne);
 console.log("User flipped " + cardTwo);
