65a447f2c46faf0648dd39d93ab38cb7b2ed5d3c
William Constantino <wconstantino@Williams-MacBook-Air.local>
2019-04-26 15:35:40 -0400

    created game logic

--- memorygame/js/main.js
+++ memorygame/js/main.js
@@ -1,15 +1,24 @@
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-var cardOne = cards[0];
-var cardTwo = cards[2];
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
-if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
-    alert("You found a match!");
-}
- else {
-        alert("Sorry, try again");
-    }
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
+function checkForMatch()	{
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+	    console.log("You found a match!");
+		}
+	 else {
+	        console.log("Sorry, try again");
+	    }
+
+}
+function flipCard(cardId)	{
+	var cardOne = cards[0];
+	var cardTwo = cards[2];
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+	if (cardsInPlay.length === 2)	{
+		checkForMatch();
+	}
+}
+
+flipCard(0)
+flipCard(2)
